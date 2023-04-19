04cc029f4ac225b43256871894f43cb0177d28c3
Marcus Vinícius da Silva Santos <marcus.vs.santos@hotmail.com>
2023-04-19 11:52:31 -0300

    Aula 28

--- crud-angular/src/app/courses/services/courses.service.ts
+++ crud-angular/src/app/courses/services/courses.service.ts
@@ -16,11 +16,11 @@ export class CoursesService {
 
   list() {
     return this.httpClient.get<Course[]>(this.API)
-    .pipe(
-      first(),
-      delay(5000),
-      tap(courses => console.log(courses))
-    );
+      .pipe(
+        first(),
+        delay(5000),
+        tap(courses => console.log(courses))
+      );
   }
 
   loadById(id: string) {
@@ -28,6 +28,19 @@ export class CoursesService {
   }
 
   save(record: Partial<Course>) {
+    if (record._id) {
+      // console.log('update');
+      return this.update(record);
+    }
+    // console.log('create');
+    return this.create(record);
+  }
+
+  private create(record: Partial<Course>) {
     return this.httpClient.post<Course>(this.API, record).pipe(first());
+  }
+
+  private update(record: Partial<Course>) {
+    return this.httpClient.put<Course>(`${this.API}/${record._id}`, record).pipe(first());
   }
 }
