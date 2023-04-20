2feff6649c503608dfad35bf251378dbe8d34ff6
Marcus Vinícius da Silva Santos <marcus.vs.santos@hotmail.com>
2023-04-20 11:35:03 -0300

    Aula 33

--- crud-angular/src/app/courses/containers/courses/courses.component.ts
+++ crud-angular/src/app/courses/containers/courses/courses.component.ts
@@ -8,6 +8,7 @@ import { catchError } from 'rxjs/operators';
 import { ErrorDialogComponent } from '../../../shared/components/error-dialog/error-dialog.component';
 import { Course } from '../../model/course';
 import { CoursesService } from '../../services/courses.service';
+import { ConfirmationDialogComponent } from 'src/app/shared/components/confirmation-dialog/confirmation-dialog.component';
 
 @Component({
   selector: 'app-courses',
@@ -60,18 +61,28 @@ export class CoursesComponent implements OnInit {
   }
 
   onRemove(course: Course){
-    this.coursesService.remove(course._id).subscribe(
-      () =>{
-        this.refresh();
-        this.snackBar.open('Curso removido com sucesso!', 'X', {
-          duration: 5000,
-          verticalPosition: 'top',
-          horizontalPosition: 'center'
-         });
-      },
-      error => this.onError('Erro ao tentar remover curso.')
 
-    );
+    const dialogRef = this.dialog.open(ConfirmationDialogComponent, {
+      data: 'Tem certeza que deseja remover esse curso?',
+    });
+
+    dialogRef.afterClosed().subscribe((result: boolean) => {
+      if(result){
+        this.coursesService.remove(course._id).subscribe(
+          () =>{
+            this.refresh();
+            this.snackBar.open('Curso removido com sucesso!', 'X', {
+              duration: 5000,
+              verticalPosition: 'top',
+              horizontalPosition: 'center'
+             });
+          },
+          error => this.onError('Erro ao tentar remover curso.')
+        );
+      }
+    });
+
+
   }
 
 }
--- crud-angular/src/app/shared/shared.module.ts
+++ crud-angular/src/app/shared/shared.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppMaterialModule } from './app-material/app-material.module';
 import { ErrorDialogComponent } from './components/error-dialog/error-dialog.component';
 import { CategoryPipe } from './pipes/category.pipe';
+import { ConfirmationDialogComponent } from './components/confirmation-dialog/confirmation-dialog.component';
 
 
 
@@ -11,7 +12,8 @@ import { CategoryPipe } from './pipes/category.pipe';
   declarations: [
 
     ErrorDialogComponent,
-     CategoryPipe
+     CategoryPipe,
+     ConfirmationDialogComponent
   ],
   imports: [
     AppMaterialModule,
