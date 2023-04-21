8e25d8e9af844ae162404335fef493f5a351edc0
Marcus Vinícius da Silva Santos <marcus.vs.santos@hotmail.com>
2023-04-21 10:50:38 -0300

    Aula 38

--- crud-angular/src/app/shared/components/confirmation-dialog/confirmation-dialog.component.ts
+++ crud-angular/src/app/shared/components/confirmation-dialog/confirmation-dialog.component.ts
@@ -1,5 +1,5 @@
 import { Component, Inject } from '@angular/core';
-import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { MatLegacyDialogRef as MatDialogRef, MAT_LEGACY_DIALOG_DATA as MAT_DIALOG_DATA } from '@angular/material/legacy-dialog';
 
 @Component({
   selector: 'app-confirmation-dialog',
